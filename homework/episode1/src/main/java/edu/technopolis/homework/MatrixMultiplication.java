c88bdcad25956f777531fe48be97cc5492f4bc7b
SobolevaAlexandra <sania.soboleva@mail.ru>
2016-10-11 18:59:06 +0300

    hw1

--- homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
+++ homework/episode1/src/main/java/edu/technopolis/homework/MatrixMultiplication.java
@@ -1,6 +1,5 @@
 package edu.technopolis.homework;
 
-import java.util.Scanner;
 
 /**
  * Matrix multiplication home task.
@@ -9,56 +8,54 @@ import java.util.Scanner;
  */
 public class MatrixMultiplication {
 
- static void init(int[][] arr,Scanner in){
-     for (int i=0;i<arr.length;i++){
-         for (int j=0;j<arr[i].length;j++) arr[i][j]=in.nextInt();;
-     }
- }
 
-    static void matrixMult(int[][] arrA, int[][] arrB, int[][] arrC){
-        for (int i=0;i<arrC.length;i++){
-            for (int j=0;j<arrC[i].length;j++)
-                for (int l=0;l<arrB.length;l++){
-                    arrC[i][j]+=arrA[i][l]*arrB[l][j];
+    static void matrixMult(int[][] arrA, int[][] arrB, int[][] arrC) {
+        for (int i = 0; i < arrC.length; i++) {
+            for (int j = 0; j < arrC[i].length; j++)
+                for (int l = 0; l < arrB.length; l++) {
+                    arrC[i][j] += arrA[i][l] * arrB[l][j];
                 }
         }
     }
 
-    static void print(int[][] arr){
-        for (int i=0;i<arr.length;i++){
-            for (int j=0;j<arr[i].length;j++) System.out.print(arr[i][j]);
+    static void print(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) System.out.print(arr[i][j]);
             System.out.println();
         }
     }
 
     public static void main(String... args) {
-        //TODO commandline input
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int x = in.nextInt();
-        int y = in.nextInt();
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int x = Integer.parseInt(args[2]);
+        int y = Integer.parseInt(args[3]);
 
-        int ind=4;
+        int ind = 4;
 
         int[][] arrA = new int[a][b];
-        init(arrA,in);
+        for (int i = 0; i < arrA.length; i++) {
+            for (int j = 0; j < arrA[i].length; j++) arrA[i][j] = Integer.parseInt(args[ind++]);
+        }
+
         int[][] arrB = new int[x][y];
-        init(arrB,in);
+
+        for (int i = 0; i < arrB.length; i++) {
+            for (int j = 0; j < arrB[i].length; j++) arrB[i][j] = Integer.parseInt(args[ind++]);
+        }
+
         int[][] arrC;
 
 
-        if (a==y) {
-            arrC=new int[x][b];
-            matrixMult(arrB,arrA,arrC);
+        if (a == y) {
+            arrC = new int[x][b];
+            matrixMult(arrB, arrA, arrC);
             print(arrC);
-        }
-        else if (b==x){
-            arrC=new int[a][y];
-            matrixMult(arrA,arrB,arrC);
+        } else if (b == x) {
+            arrC = new int[a][y];
+            matrixMult(arrA, arrB, arrC);
             print(arrC);
-        }
-        else System.out.println("Нельзя перемножить матрицы размером"+a+" на "+b+" и "+x+" на "+y);
+        } else System.out.println("Нельзя перемножить матрицы размером" + a + " на " + b + " и " + x + " на " + y);
 
     }
 }
